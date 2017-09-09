958866a01c9983945fd5527e13b94ea8771c28a5
Juan Leni <lenijuan@gmail.com>
2017-09-09 08:59:15 +0200

    Adding flushdata.sh

---
+++ flushdata.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+rm -rf ./volumes/node1/.qrl/data
+rm -rf ./volumes/node2/.qrl/data
+rm -rf ./volumes/node3/.qrl/data
+rm -rf ./volumes/node4/.qrl/data
+rm -rf ./volumes/node5/.qrl/data
+
+rm -rf ./volumes/node1/.qrl/wallet
+rm -rf ./volumes/node2/.qrl/wallet
+rm -rf ./volumes/node3/.qrl/wallet
+rm -rf ./volumes/node4/.qrl/wallet
+rm -rf ./volumes/node5/.qrl/wallet
+
+rm ./volumes/node1/.qrl/output
+rm ./volumes/node2/.qrl/output
+rm ./volumes/node3/.qrl/output
+rm ./volumes/node4/.qrl/output
+rm ./volumes/node5/.qrl/output
